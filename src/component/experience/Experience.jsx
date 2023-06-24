91a01e70d18137ac153ef32a49b03aac9ce79133
fabriceMokfembam <fabricemokfembam@.com>
2023-06-24 05:58:26 -0700

    odifications

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -1,58 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./Experience.css";
-// import {FaAward} from 'react-icons/fa'
-// import {FiUsers} from 'react-icons/fi'
-// import {VscFolderLibrary} from 'react-icons/vsc'
-
 
 export const Experience = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      delay: 200,
+    });
+  }, []);
+
   return (
     <section id="experience">
-      <h5>What skills i offer</h5>
+      <h5>What skills I offer</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
-      <article className="about__card">
-              <div className="text">HTML5</div>
-                <h5>3+ years Experience</h5>
-                <small>30+  projects Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">CSS3</div>
-                <h5>3+  years Experience</h5>
-                <small>30+  projects Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">JavaScript</div>
-                <h5>1+  years Experience</h5>
-                <small>10+ Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">Bootstrap</div>
-                <h5>1+  years Experience</h5>
-                <small>2  projects Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">React.js</div>
-                <h5>1month+  years Experience</h5>
-                <small>1  project Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">C/C++</div>
-                <h5>3+  years Experience</h5>
-                <small>50+  exercises Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">Java</div>
-                <h5>1+  years Experience</h5>
-                <small>2 projects Completed</small>
-              </article>
-              <article className="about__card">
-              <div className="text">SQL</div>
-                <h5>2+  years Experience</h5>
-                <small>20+  exercises Completed</small>
-              </article>
+        <article className="about__card" data-aos="zoom-in">
+          <div className="text">HTML5</div>
+          <h5>3+ years Experience</h5>
+          <small>30+ projects Completed</small>
+        </article>
+        <article className="about__card" data-aos="fade-up">
+          <div className="text">CSS3</div>
+          <h5>3+ years Experience</h5>
+          <small>30+ projects Completed</small>
+        </article>
+        <article className="about__card" data-aos="zoom-in">
+          <div className="text">JavaScript</div>
+          <h5>1+ years Experience</h5>
+          <small>10+ Completed</small>
+        </article>
+        <article className="about__card" data-aos="fade-down">
+          <div className="text">Bootstrap</div>
+          <h5>1+ years Experience</h5>
+          <small>2 projects Completed</small>
+        </article>
+        <article className="about__card" data-aos="zoom-in">
+          <div className="text">React.js</div>
+          <h5>1month+ years Experience</h5>
+          <small>1 project Completed</small>
+        </article>
+        <article className="about__card" data-aos="fade-right">
+          <div className="text">C/C++</div>
+          <h5>3+ years Experience</h5>
+          <small>50+ exercises Completed</small>
+        </article>
+        <article className="about__card" data-aos="zoom-in">
+          <div className="text">Java</div>
+          <h5>1+ years Experience</h5>
+          <small>2 projects Completed</small>
+        </article>
+        <article className="about__card" data-aos="fade-left">
+          <div className="text">SQL</div>
+          <h5>2+ years Experience</h5>
+          <small>20+ exercises Completed</small>
+        </article>
       </div>
     </section>
   );
 };
+
 export default Experience;
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,24 +1,131 @@
-import React from 'react'
-import './header.css'
-import './CTA'
-import CTA from './CTA'
-import me from '../assets/hack.jpg'
-import HeaderSocial from './HeaderSocial'
+import React, { useState, useEffect } from 'react';
+import './header.css';
+import './CTA';
+import CTA from './CTA';
+import me from '../assets/hack.jpg';
+import HeaderSocial from './HeaderSocial';
+import { motion } from 'framer-motion';
+
+const TYPING_DELAY = 150;
+const TEXTS = ['software engineer', 'ui/ux designer', 'tutor', 'mentor'];
+
 export const Header = () => {
-return (
-<header>
-        <div className='container header__container'>
-        <h5>Hello I'm</h5>
-        <h1>Fabrice-Mokfenbam</h1>
-        <h5 className='text-light'>Software Engineer </h5>
-        <CTA/>
-        <HeaderSocial/>
-        <div className='my_image'>
-        <img src={me} alt='my image'/>
-        </div>
-        <a href='#contact' className='scroll__down'>Scroll down</a>
-        </div>
- </header>
-)
-}
+  const [typingText, setTypingText] = useState('');
+  const [textIndex, setTextIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentIndex = textIndex % TEXTS.length;
+      const currentText = TEXTS[currentIndex];
+
+      if (isDeleting) {
+        setTypingText((prev) => prev.slice(0, -1));
+      } else {
+        setTypingText((prev) => {
+          const nextCharIndex = prev.length;
+          const nextChar = currentText[nextCharIndex];
+          if (nextChar === undefined) {
+            clearInterval(intervalId);
+            return prev;
+          }
+          return prev + nextChar;
+        });
+      }
+
+      const isComplete = isDeleting
+        ? typingText.length === 0
+        : typingText === currentText;
+
+      if (isComplete) {
+        setIsDeleting((prev) => !prev);
+        setTextIndex((prev) => prev + 1);
+      }
+    }, TYPING_DELAY);
+
+    return () => clearInterval(intervalId);
+  }, [textIndex, typingText, isDeleting]);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { delay: 0.5, duration: 2 } },
+  };
+
+  const imageVariants = {
+    hidden: { x: '100vw' },
+    visible: { x: 0, transition: { delay: 1, duration: 2 } },
+  };
+
+  const buttonVariants = {
+    hidden: { scale: 0 },
+    visible: { scale: 1, transition: { delay: 1, duration: 2 } },
+  };
+
+  const textLighterVariants = {
+    hidden: {
+      opacity: 0,
+      y: -50,
+      rotate: -180,
+      skewY: 10,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      rotate: 0,
+      skewY: 0,
+      transition: {
+        delay: 2,
+        duration: 2,
+        type: 'spring',
+        stiffness: 100,
+        damping: 10,
+      },
+    },
+  };
+
+  return (
+    <header>
+      <div className='container header__container'>
+        <motion.div
+          className='hero-section'
+          variants={containerVariants}
+          initial='hidden'
+          animate='visible'
+        >
+          <h5>Hello I'm</h5>
+          <h1>Fabrice-Mokfenbam</h1>
+          <motion.h5 className='text-light' animate={{ text: typingText }}>
+            {typingText}
+          </motion.h5>
+          <motion.h5
+            className='text-lighter'
+            variants={textLighterVariants}
+            initial='hidden'
+            animate='visible'
+          >
+            I'm a tech enthusiast and a complete lover of problem solving in a simplified and organized matter such that you can be satisfied.
+          </motion.h5>
+          <HeaderSocial />
+          <motion.button
+            className='contact__button'
+            variants={buttonVariants}
+            initial='hidden'
+            animate='visible'
+          >
+            Contact Me
+          </motion.button>
+        </motion.div>
+        <motion.div
+          className='my_image'
+          variants={imageVariants}
+          initial='hidden'
+          animate='visible'
+        >
+          <img src={me} alt='my image' />
+        </motion.div>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
